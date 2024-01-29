21056e12d226c2db3f3880ab046ed5805c604875
Spartaquee <shoxieq@gmail.com>
2024-01-29 14:48:34 +0500

    Initial commit

---
+++ src/main/java/com/example/laborator/LaboratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.laborator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorApplication.class, args);
+    }
+
+}
